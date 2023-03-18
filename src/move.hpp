1bbf181bc0cf6e7ba4b2a465cc1eeffe0fc481ac
MoriartyProfessor <farididdin456@gmail.com>
2023-03-18 22:27:26 +0500

    Finish most of move class functionality.

--- src/move.hpp
+++ src/move.hpp
@@ -12,17 +12,59 @@ class Move
     Move() = default;
     Move(uint32_t move);
 
+    Move& operator= (Move other);
+    // either don't take into account unused bits, or make sure they are always set to zero
+    bool operator== (Move other) const;   
+    bool operator!= (Move other) const;
+
     Square from() const;
     Square to() const;
     PieceType piece_type() const;
 
+    PieceType capture_piece_type(PieceType type) const;
+    PieceType promotion_piece_type(PieceType type) const;
+
+    bool is_quite() const;
+    bool is_capture() const;
+    bool is_promotion() const;
+    bool is_double_pawn_push() const;
+    bool is_king_side_castle() const;
+    bool is_queen_side_castle() const;
+    bool is_en_passant() const;
+
     void set_from(Square from);
     void set_to(Square to);
-    void set_piece_type(PieceType from);
+    void set_piece_type(PieceType type);
 
-    std::string notation() const;
+    void set_capture_piece_type(PieceType type);
+    void set_promotion_piece_type(PieceType type);
+
+    void set_quite();
+    void set_capture();
+    void set_promotion();
+    void set_double_pawn_push();
+    void set_king_side_castle();
+    void set_queen_side_castle();
+    void set_en_passant();
+
+    std::string descriptive_notation() const;
+    std::string algebraic_notation() const;
 
     private:
+
+    enum FlagBit
+    {
+        QUITE,
+        CAPTURE,
+        PROMOTION,
+        DOUBLE_PAWN_PUSH,
+        KING_CASTLE,
+        QUEEN_CASTLE,
+        EN_PASSANT
+    };
+
+    void set_flag(FlagBit flag);
+
     uint32_t move_ = 0;
 };
 
