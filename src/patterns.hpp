88a1a4bfdb6d3f37f62be8206b18aceb8cadb0a8
MoriartyProfessor <farididdin456@gmail.com>
2023-03-27 06:36:09 +0500

    Add Position::is_in_check to interface. Aesthetic changes.

--- src/patterns.hpp
+++ src/patterns.hpp
@@ -1,4 +1,9 @@
 #ifndef ALTAIR_PATTERNS_HPP
 #define ALTAIR_PATTERNS_HPP
 
+#include "defs.hpp"
+
+BitBoard knight_attacks[N_SQUARES];
+BitBoard king_attacks[N_SQUARES];
+
 #endif // ALTAIR_PATTERNS_HPP
