fff386126bd407463ec45e5e6e0878fea6adc064
MoriartyProfessor <farididdin456@gmail.com>
2023-04-11 17:36:30 +0500

    Add knight move generation.

--- src/movegenerator.hpp
+++ src/movegenerator.hpp
@@ -20,7 +20,15 @@ class MoveGenerator
     std::vector<Move> legal_moves_;
     std::vector<Move> pseudolegal_moves_;
 
+    private:
+    void gen_legal_moves_();
+    void gen_pseudolegal_moves_();
+
     /* Convenience functions for generating moves for individual pieces and special cases */
+    void gen_knight_moves_();
+    void gen_king_moves_();
+
+    void add_piece_moves_(PieceType type, Square from, BitBoard attacks);
 };
 
 #endif // ALTAIR_MOVEGENERATOR_HPP
