355e931a869433fcb3d3aa714d20263aefcf744f
MoriartyProfessor <farididdin456@gmail.com>
2023-04-11 17:35:15 +0500

    Add King and Knight attack patterns.

--- src/patterns.cpp
+++ src/patterns.cpp
@@ -1,1 +1,77 @@
 #include "patterns.hpp"
+#include "bitboard.hpp"
+
+namespace Patterns
+{
+    BitBoard knight_attacks_[N_SQUARES];
+    BitBoard king_attacks_[N_SQUARES];
+
+    BitBoard gen_knight_attacks_(Square square)
+    {
+        BitBoard knight_bitboard = BitBoards::square_set_in(square);
+        BitBoard knight_attacks = EMPTY_BB;
+
+        knight_attacks |= (knight_bitboard << 17) & (~FILE_A_BB)           ;
+        knight_attacks |= (knight_bitboard << 10) & (~(FILE_A_BB|FILE_B_BB))  ;
+        knight_attacks |= (knight_bitboard >>  6) & (~(FILE_A_BB|FILE_B_BB))  ;
+        knight_attacks |= (knight_bitboard >> 15) & (~FILE_A_BB)           ;
+        knight_attacks |= (knight_bitboard << 15) & (~FILE_H_BB)           ;
+        knight_attacks |= (knight_bitboard <<  6) & (~(FILE_G_BB|FILE_H_BB))  ;
+        knight_attacks |= (knight_bitboard >> 10) & (~(FILE_G_BB|FILE_H_BB))  ;
+        knight_attacks |= (knight_bitboard >> 17) & (~FILE_H_BB)           ;
+
+        return knight_attacks;
+    }
+
+    BitBoard gen_king_attacks_(Square square)
+    {
+        BitBoard king_bitboard = BitBoards::square_set_in(square);
+        BitBoard king_attacks = EMPTY_BB;
+
+        king_attacks |= BitBoards::north       (king_bitboard);
+        king_attacks |= BitBoards::north_west  (king_bitboard);
+        king_attacks |= BitBoards::west        (king_bitboard);
+        king_attacks |= BitBoards::south_west  (king_bitboard);
+        king_attacks |= BitBoards::south       (king_bitboard);
+        king_attacks |= BitBoards::south_east  (king_bitboard);
+        king_attacks |= BitBoards::east        (king_bitboard);
+        king_attacks |= BitBoards::north_east  (king_bitboard);
+
+        return king_attacks;
+    }
+
+
+
+    void init_knight_attacks_()
+    {
+        for(Square square = SQ_A1; square < N_SQUARES; ++square)
+        {
+            knight_attacks_[square] = gen_knight_attacks_(square);
+        }
+    }
+
+    void init_king_attacks_()
+    {
+        for(Square square = SQ_A1; square < N_SQUARES; ++square)
+        {
+            king_attacks_[square] = gen_king_attacks_(square);
+        }
+    }
+
+
+    void init()
+    {
+        init_knight_attacks_();
+        init_king_attacks_();
+    }
+
+    BitBoard get_knight_attacks(Square square)
+    {
+        return knight_attacks_[square];
+    }
+
+    BitBoard get_king_attacks(Square square)
+    {
+        return king_attacks_[square];
+    }
+}
--- src/patterns.hpp
+++ src/patterns.hpp
@@ -5,8 +5,15 @@
 
 namespace Patterns
 {
-    BitBoard knight_attacks[N_SQUARES];
-    BitBoard king_attacks[N_SQUARES];
+    void init();
+    /* Think about pawn pattern interfaces later */ 
+    BitBoard get_pawn_pushes(Color color, Square square);
+
+    BitBoard get_knight_attacks(Square square);
+    BitBoard get_bishop_attacks(Square square, BitBoard occupancy_bitboard);
+    BitBoard get_rook_attacks(Square square, BitBoard occupancy_bitboard);
+    BitBoard get_queen_attacks(Square square, BitBoard occupancy_bitboard);
+    BitBoard get_king_attacks(Square square);
 }
 
 #endif // ALTAIR_PATTERNS_HPP
