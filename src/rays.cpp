00bfe1f541d2e055b5f8eba9f8274b99bc7fb4c5
MoriartyProfessor <farididdin456@gmail.com>
2023-04-15 17:45:22 +0500

    Fix west ray generation. Rename to gen_diagonals_.

--- src/rays.cpp
+++ src/rays.cpp
@@ -6,13 +6,13 @@ BitBoard rays[N_DIRECTIONS][N_SQUARES];
 namespace Rays
 {
     template<Directions direction>
-    BitBoard gen_diagonals(Square square)
+    BitBoard gen_diagonals_(Square square)
     {
         static_assert(direction == NORTH_WEST || direction == SOUTH_WEST || direction == SOUTH_EAST || direction == NORTH_EAST );
         return EMPTY_BB;
     }
     template<>
-    BitBoard gen_diagonals<NORTH_WEST>(Square square)
+    BitBoard gen_diagonals_<NORTH_WEST>(Square square)
     {
         BitBoard north_west_BB = MAIN_ANTIDIAGONAL_BB;
         File file = get_file(square);
@@ -23,7 +23,7 @@ namespace Rays
         return north_west_BB;
     }
     template<>
-    BitBoard gen_diagonals<SOUTH_WEST>(Square square)
+    BitBoard gen_diagonals_<SOUTH_WEST>(Square square)
     {
         BitBoard south_west_BB = MAIN_DIAGONAL_BB;
         File file = get_file(square);
@@ -34,7 +34,7 @@ namespace Rays
         return south_west_BB;
     }
     template<>
-    BitBoard gen_diagonals<SOUTH_EAST>(Square square)
+    BitBoard gen_diagonals_<SOUTH_EAST>(Square square)
     {
         BitBoard south_east_BB = MAIN_ANTIDIAGONAL_BB;
         File file = get_file(square) + 1;
@@ -45,7 +45,7 @@ namespace Rays
         return south_east_BB;
     }
     template<>
-    BitBoard gen_diagonals<NORTH_EAST>(Square square)
+    BitBoard gen_diagonals_<NORTH_EAST>(Square square)
     {
         /* Much slower than looping through squares and calculating on fly, but it is precalculated so we should be fine */
         BitBoard north_east_BB = MAIN_DIAGONAL_BB;
@@ -56,6 +56,7 @@ namespace Rays
 
         return north_east_BB;
     }
+    
 
     void init()
     {
@@ -70,15 +71,15 @@ namespace Rays
 
             rays[EAST][square] = ((1ULL << (square | 7)) - (1ULL << square)) << 1;
 
-            rays[WEST][N_SQUARES - square - 1] = (1ULL << square) - (1ULL << ((square & ~7)));
+            rays[WEST][square] = (1ULL << square) - (1ULL << ((square & ~7)));
 
-            rays[NORTH_WEST][square] = gen_diagonals<NORTH_WEST>(square);
+            rays[NORTH_WEST][square] = gen_diagonals_<NORTH_WEST>(square);
 
-            rays[SOUTH_WEST][square] = gen_diagonals<SOUTH_WEST>(square);
+            rays[SOUTH_WEST][square] = gen_diagonals_<SOUTH_WEST>(square);
 
-            rays[SOUTH_EAST][square] = gen_diagonals<SOUTH_EAST>(square);
+            rays[SOUTH_EAST][square] = gen_diagonals_<SOUTH_EAST>(square);
 
-            rays[NORTH_EAST][square] = gen_diagonals<NORTH_EAST>(square);
+            rays[NORTH_EAST][square] = gen_diagonals_<NORTH_EAST>(square);
         }
     }
 
