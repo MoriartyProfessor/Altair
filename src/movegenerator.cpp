4800746bb407b7d106687d58d0513ed404823945
MoriartyProfessor <farididdin456@gmail.com>
2023-04-24 00:00:52 +0500

    Fix and refactor move generation.

--- src/movegenerator.cpp
+++ src/movegenerator.cpp
@@ -3,77 +3,67 @@
 #include "bitboard.hpp"
 #include "patterns.hpp"
 
-MoveGenerator::MoveGenerator(const Position& position)
-: position_{position}
+#include <iostream>
+
+
+std::vector<Move> MoveGenerator::all_moves(const Position& position)
 {
-    gen_pseudolegal_moves_();
+    gen_moves_<Config::GENERATE_ALL>(position);
+    return all_moves_;
 }
 
-void MoveGenerator::set_position(const Position& position)
+std::vector<Move> MoveGenerator::tactical_moves(const Position& position)
 {
-    position_ = position;
-    gen_pseudolegal_moves_();
-}
-
-
-std::vector<Move> MoveGenerator::legal_moves()
-{
-    /* Generate legal moves only on-demand */
-    gen_legal_moves_();
-    return legal_moves_;
-}
-
-std::vector<Move> MoveGenerator::pseudolegal_moves()
-{
-    return pseudolegal_moves_;
+    gen_moves_<Config::GENERATE_TACTICAL>(position);
+    return tactical_moves_;
 }
 
 
-
-void MoveGenerator::gen_legal_moves_()
+template<MoveGenerator::Config config>
+void MoveGenerator::gen_moves_(const Position& position)
 {
-
+    gen_piece_moves_<config, KNIGHT >(position);
+    gen_piece_moves_<config, BISHOP >(position);
+    gen_piece_moves_<config, ROOK   >(position);
+    gen_piece_moves_<config, QUEEN  >(position);
+    gen_piece_moves_<config, KING   >(position);
 }
 
-void MoveGenerator::gen_pseudolegal_moves_()
+
+template<MoveGenerator::Config config, PieceType piece_type>
+void MoveGenerator::gen_piece_moves_(const Position& position)
 {
-    gen_knight_moves_();
-    gen_king_moves_();
-}
+    static_assert(piece_type < N_PIECE_TYPES);
+    BitBoard piece_bitboard = position.piece_bitboard(position.side_to_move(), piece_type);
 
-
-void MoveGenerator::gen_knight_moves_()
-{
-    BitBoard knight_bitboard = position_.piece_bitboard(position_.side_to_move(), KNIGHT);
-
-    for(Square knight = pop_LSB(knight_bitboard); knight != N_SQUARES; knight = pop_LSB(knight_bitboard))
+    for(Square piece_sq = pop_LSB(piece_bitboard); piece_sq != N_SQUARES; piece_sq = pop_LSB(piece_bitboard))
     {
-        BitBoard knight_attacks = Patterns::get_knight_attacks(knight) & ~position_.occupancy_bitboard(position_.side_to_move());
-        add_piece_moves_(KNIGHT, knight, knight_attacks);
-    }
-}
-
-void MoveGenerator::gen_king_moves_()
-{
-    /* Maybe generalize with other gen functions */
-    BitBoard king_bitboard = position_.piece_bitboard(position_.side_to_move(), KING);
-
-    for(Square king = pop_LSB(king_bitboard); king != N_SQUARES; king = pop_LSB(king_bitboard))
-    {
-        BitBoard king_attacks = Patterns::get_king_attacks(king) & ~position_.occupancy_bitboard(position_.side_to_move());
-        add_piece_moves_(KING, king, king_attacks);
+        BitBoard piece_attacks = EMPTY_BB;
+        if constexpr (piece_type == KNIGHT)
+            piece_attacks = Patterns::get_knight_attacks(piece_sq) & ~position.occupancy_bitboard(position.side_to_move());
+        else if (piece_type == BISHOP)
+            piece_attacks = Patterns::get_bishop_attacks(piece_sq, position.occupancy_bitboard()) & ~position.occupancy_bitboard(position.side_to_move());
+        else if (piece_type == ROOK)
+            piece_attacks = Patterns::get_rook_attacks(piece_sq, position.occupancy_bitboard()) & ~position.occupancy_bitboard(position.side_to_move());
+        else if (piece_type == QUEEN)
+            piece_attacks = Patterns::get_queen_attacks(piece_sq, position.occupancy_bitboard()) & ~position.occupancy_bitboard(position.side_to_move());
+        else if (piece_type == KING)
+            piece_attacks = Patterns::get_king_attacks(piece_sq) & ~position.occupancy_bitboard(position.side_to_move());
+        
+        add_piece_moves_<config, piece_type>(position, piece_sq, piece_attacks);
     }
 }
 
 
-void MoveGenerator::add_piece_moves_(PieceType type, Square from, BitBoard attacks)
+template<MoveGenerator::Config config, PieceType piece_type>
+void MoveGenerator::add_piece_moves_(const Position& position, Square from, BitBoard attacks)
 {
     for(Square attack = pop_LSB(attacks); attack != N_SQUARES; attack = pop_LSB(attacks))
     {
         Move move{from, attack};
-        move.set_piece_type(type);
+        move.set_piece_type(piece_type);
 
-        Piece attacked_piece = position_.piece_occupying(attack);
+        Piece attacked_piece = position.piece_occupying(attack);
         if(attacked_piece != N_PIECES)
         {
             move.set_capture();
@@ -84,6 +74,9 @@ void MoveGenerator::add_piece_moves_(PieceType type, Square from, BitBoard attac
             move.set_quite();
         }
 
-        pseudolegal_moves_.push_back(move);
+        if(config == Config::GENERATE_ALL)
+            all_moves_.push_back(move);
+        if(config == Config::GENERATE_TACTICAL)
+            tactical_moves_.push_back(move);
     }
 }
