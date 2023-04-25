243a35b0a70f167651969c93b60d77d48a57b054
MoriartyProfessor <farididdin456@gmail.com>
2023-04-25 05:49:35 +0500

    Add prelimenary perft. Add MAX_DEPTH.

--- src/perft.cpp
+++ src/perft.cpp
@@ -1,6 +1,23 @@
 #include "perft.hpp"
+#include "defs.hpp"
+#include "movegenerator.hpp"
 
-uint64_t Perft::perft(const Position& position, uint32_t depth)
+MoveGenerator move_generator;
+Position::IrrecoverableState irrecoverable_states[MAX_DEPTH];
+
+uint64_t Perft::test(Position& position, uint32_t depth)
 {
-    
+    if(depth == 0)
+        return 1;
+    uint64_t node_count = 0;
+    auto moves = move_generator.all_moves(position);
+    for(auto move : moves)
+    {
+        irrecoverable_states[depth] = position.irrecoverable_state();
+        position.make_move(move);
+        if(!position.is_in_check(toggle_color(position.side_to_move())))
+            node_count += test(position, depth - 1);
+        position.unmake_move(move, irrecoverable_states[depth]);
+    }
+    return node_count;
 }
