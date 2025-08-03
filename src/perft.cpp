cdbcfb56a659ab0ad5bc3f665dc4b7d4e27b08a9
MoriartyProfessor <farididdin456@gmail.com>
2025-08-03 07:08:30 +0500

    More thorough perft test.

--- src/perft.cpp
+++ src/perft.cpp
@@ -7,23 +7,57 @@
 
 Position::IrrecoverableState irrecoverable_states_[MAX_DEPTH];
 
-uint64_t Perft::test(Position& position, uint32_t depth)
+uint64_t Perft::test(Position &position, uint32_t depth)
 {
     std::vector<Move> moves;
     moves.reserve(256);
-    if(depth == 0)
+    if (depth == 0)
         return 1;
 
     MoveGenerator move_generator_{&position, &moves};
     move_generator_.gen_all_moves();
     uint64_t node_count = 0;
-    for(auto move : moves)
+    for (auto move : moves)
     {
         irrecoverable_states_[depth] = position.irrecoverable_state();
         position.make_move(move);
-        if(!position.is_in_check(toggle_color(position.side_to_move())))
+        if (!position.is_in_check(toggle_color(position.side_to_move())))
             node_count += test(position, depth - 1);
         position.unmake_move(move, irrecoverable_states_[depth]);
     }
     return node_count;
+}
+
+void Perft::thorough_test(Position &position, Result &result, Move move, uint32_t depth)
+{
+    if (depth == 0)
+    {
+        ++result.nodes;
+        if (move.is_capture())
+            ++result.captures;
+        if (move.is_en_passant())
+            ++result.en_passants;
+        if (move.is_king_side_castle() || move.is_queen_side_castle())
+            ++result.castles;
+        if (move.is_promotion())
+            ++result.promotions;
+        if (position.is_in_check(position.side_to_move()))
+            ++result.checks;
+        return;
+    }
+
+    std::vector<Move> moves;
+    moves.reserve(256);
+
+    MoveGenerator move_generator_{&position, &moves};
+    move_generator_.gen_all_moves();
+
+    for (auto move : moves)
+    {
+        irrecoverable_states_[depth] = position.irrecoverable_state();
+        position.make_move(move);
+        if (!position.is_in_check(toggle_color(position.side_to_move())))
+            thorough_test(position, result, move, depth - 1);
+        position.unmake_move(move, irrecoverable_states_[depth]);
+    }
 }
--- src/perft.hpp
+++ src/perft.hpp
@@ -5,7 +5,17 @@
 
 namespace Perft
 {
-    uint64_t test(Position& position, uint32_t depth);
+    struct Result
+    {
+        uint64_t nodes = 0;
+        uint64_t captures = 0;
+        uint64_t en_passants = 0;
+        uint64_t castles = 0;
+        uint64_t promotions = 0;
+        uint64_t checks = 0;
+    };
+    uint64_t test(Position &position, uint32_t depth);
+    void thorough_test(Position &position, Result &result, Move move, uint32_t depth);
 }
 
 #endif // ALTAIR_PERFT_HPP
