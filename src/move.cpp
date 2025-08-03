0851496a4600cefb6cb28a1be3329da17b06096d
MoriartyProfessor <farididdin456@gmail.com>
2025-08-03 07:08:57 +0500

    Move can have multiple flags at the same time.

--- src/move.cpp
+++ src/move.cpp
@@ -106,7 +106,6 @@ void Move::set_en_passant()         { set_flag(EN_PASSANT); }
 void Move::set_flag(FlagBit flag)
 {
     uint32_t mask = 0x7F << 21;
-    move_ &= ~mask;
     move_ |= mask & (1 << (21 + flag));
 }
 
