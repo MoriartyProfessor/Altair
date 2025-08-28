f3ccdf2ca06c3794154d7e76b10701a6417b7cf0
MoriartyProfessor <farididdin456@gmail.com>
2025-08-28 15:36:23 +0500

    Most basic eval.

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,1 +1,21 @@
 #include "eval.hpp"
+namespace Evaluation
+{
+    constexpr int32_t MATERIAL_VALUES[N_PIECE_TYPES] = {100, 350, 350, 525, 1000, 20000};
+
+    int32_t evaluate(const Position &position)
+    {
+        int32_t eval = 0;
+        eval += MATERIAL_VALUES[PAWN]   * (position.piece_count(WH_PAWN)   - position.piece_count(BL_PAWN));
+        eval += MATERIAL_VALUES[KNIGHT] * (position.piece_count(WH_KNIGHT) - position.piece_count(BL_KNIGHT));
+        eval += MATERIAL_VALUES[BISHOP] * (position.piece_count(WH_BISHOP) - position.piece_count(BL_BISHOP));
+        eval += MATERIAL_VALUES[ROOK]   * (position.piece_count(WH_ROOK)   - position.piece_count(BL_ROOK));
+        eval += MATERIAL_VALUES[QUEEN]  * (position.piece_count(WH_QUEEN)  - position.piece_count(BL_QUEEN));
+        eval += MATERIAL_VALUES[KING]   * (position.piece_count(WH_KING)   - position.piece_count(BL_KING));
+
+        /*Pick a better name?*/
+        int32_t side_to_move = position.side_to_move() == WHITE ? 1 : -1;
+        
+        return eval * side_to_move;
+    }
+}
--- src/eval.hpp
+++ src/eval.hpp
@@ -1,4 +1,11 @@
 #ifndef ALTAIR_EVAL_HPP
 #define ALTAIR_EVAL_HPP
 
+#include "position.hpp"
+
+namespace Evaluation
+{
+    int32_t evaluate(const Position& position);
+}
+
 #endif // ALTAIR_EVAL_HPP
