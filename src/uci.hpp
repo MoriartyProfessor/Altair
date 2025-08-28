9138de1bb4730eefd3d0d90fcc2458949bee67e7
MoriartyProfessor <farididdin456@gmail.com>
2025-08-28 15:35:07 +0500

    Init UCI

--- src/uci.hpp
+++ src/uci.hpp
@@ -1,4 +1,35 @@
 #ifndef ALTAIR_UCI_HPP
 #define ALTAIR_UCI_HPP
 
+#include "search.hpp"
+
+#include <iostream>
+
+namespace UCI
+{
+    class Game
+    {
+    public:
+        Game() : output_stream_{std::cout}, input_stream_{std::cin}
+        {
+        }
+        void start();
+
+    private:
+        void execute_uci_command_();
+        void execute_isready_command_();
+        void execute_ucinewgame_command_();
+        void execute_position_command_(std::istringstream &command);
+        void execute_go_command_(std::istringstream &command);
+        void execute_stop_command_(std::istringstream &command);
+        void execute_quit_command_(std::istringstream &command);
+
+    private:
+        std::ostream &output_stream_;
+        std::istream &input_stream_;
+        Position position_;
+        Move best_move_;
+    };
+}
+
 #endif // ALTAIR_UCI_HPP
