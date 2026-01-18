0fbbfb0a40ea90d9c037e25c0d488101a6145a84
MoriartyProfessor <farididdin456@gmail.com>
2026-01-18 18:42:49 +0500

    MoveOrderer skeleton.

--- src/moveorderer.hpp
+++ src/moveorderer.hpp
@@ -1,4 +1,23 @@
 #ifndef ALTAIR_MOVEORDERER_HPP
 #define ALTAIR_MOVEORDERER_HPP
 
+#include "movegenerator.hpp"
+
+#include <vector>
+
+class MoveOrderer
+{
+    public:
+    MoveOrderer(MoveList* moves);
+    Move next();
+    bool has_next() const;
+
+    private:
+    void assign_scores_();
+
+    MoveList& moves_;
+    std::vector<uint32_t> scores_;
+    std::uint16_t current_index_ = 0;
+};
+
 #endif // ALTAIR_MOVEORDERER_HPP
