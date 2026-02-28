07b8c742b49182871c655383497e9036f1635582
MoriartyProfessor <farididdin456@gmail.com>
2026-02-28 13:03:08 +0500

    MoveOrderer test.

---
+++ tests/moveorderer.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+
+#include "movegenerator.hpp"
+#include "moveorderer.hpp"
+#include "patterns.hpp"
+#include "position.hpp"
+#include "rays.hpp"
+
+TEST(moveorderer, basic) {
+    Rays::init();
+    Patterns::init();
+
+    Position position{"r3k2r/p1ppqpb1/bn1Ppnp1/4N3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"};
+
+    MoveList moves;
+
+    MoveGenerator move_generator{&position, &moves};
+    move_generator.gen_all_moves();
+
+    MoveOrderer move_orderer{moves};
+
+    Move first_move = move_orderer.next();
+    EXPECT_EQ(first_move.from(), SQ_D6);
+    EXPECT_EQ(first_move.to(), SQ_E7);
+    EXPECT_EQ(first_move.is_capture(), true);
+    EXPECT_EQ(first_move.capture_piece_type(), PieceTypes::QUEEN);
+
+    Move second_move = move_orderer.next();
+    EXPECT_EQ(second_move.from(), SQ_E2);
+    EXPECT_EQ(second_move.to(), SQ_A6);
+    EXPECT_EQ(second_move.is_capture(), true);
+    EXPECT_EQ(second_move.capture_piece_type(), PieceTypes::BISHOP);
+
+    Move third_move = move_orderer.next();
+    EXPECT_EQ(third_move.from(), SQ_F3);
+    EXPECT_EQ(third_move.to(), SQ_F6);
+    EXPECT_EQ(third_move.is_capture(), true);
+    EXPECT_EQ(third_move.capture_piece_type(), PieceTypes::KNIGHT);
+}
