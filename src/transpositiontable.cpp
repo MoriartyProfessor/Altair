736216de739f2d250329c86c6e8680cb6f56a7ed
MoriartyProfessor <farididdin456@gmail.com>
2026-03-03 17:00:30 +0500

    Init transposition table.

--- src/transpositiontable.cpp
+++ src/transpositiontable.cpp
@@ -1,1 +1,49 @@
 #include "transpositiontable.hpp"
+
+#include "utils.hpp"
+
+#include <stdexcept>
+
+TranspostionTable::TranspostionTable(uint64_t size) : size_{size} 
+{
+    if(!is_power_of_two(size))
+        throw std::runtime_error("TT only supports size of power of two");
+    table_.resize(size_);
+    hash_mask_ = std::numeric_limits<uint64_t>::max();
+    hash_mask_ >>= 64 - LSB(size);
+}
+
+void TranspostionTable::insert(TTEntry&& tt_entry)
+{
+    auto& bucket = table_[get_partial_key(tt_entry.key)];
+    for(auto &bucket_entry : bucket)
+    {
+        // Always replace policy for tier-2 collisions
+        if(bucket_entry.key == tt_entry.key)
+        {
+            bucket_entry = std::move(tt_entry);
+            return;
+        }
+    }
+    // LRU policy when bucket is full
+    if(bucket.size() >= BUCKET_SIZE - 1)
+        bucket[0] = std::move(tt_entry);
+    else
+        bucket.push_back(std::move(tt_entry));
+}
+
+std::optional<TTEntry> TranspostionTable::probe(Zobrist::HashKey key) const
+{
+    const auto& bucket = table_[get_partial_key(key)];
+    for(const auto& entry : bucket)
+    {
+        if(entry.key == key)
+            return entry;
+    }
+    return std::nullopt;
+}
+
+uint64_t TranspostionTable::get_partial_key(Zobrist::HashKey key) const
+{
+    return hash_mask_ & key;
+}
