dc9fd7af88a3ad3eb57d3cb72f080b670e00d1ff
MoriartyProfessor <farididdin456@gmail.com>
2026-03-08 04:46:58 +0500

    Basic and dirty alpha-beta.

--- src/search.cpp
+++ src/search.cpp
@@ -1,6 +1,8 @@
 #include "search.hpp"
 #include "movegenerator.hpp"
+#include "moveorderer.hpp"
 #include "eval.hpp"
+#include "transpositiontable.hpp"
 
 #include <limits>
 #include <iostream>
@@ -8,77 +10,101 @@
 
 namespace Search
 {
-    constexpr uint8_t SEARCH_DEPTH = 6;
+    constexpr uint8_t SEARCH_DEPTH = 9;
     constexpr int32_t STALEMATE_SCORE = 0;
     constexpr int32_t MATE_SCORE = -15000;
 
     Position::IrrecoverableState irrecoverable_states_[MAX_DEPTH];
 
-    int32_t negamax_(Position &position, int current_depth)
+    TranspostionTable transposition_table{4_M};
+
+    struct Result
     {
-        if (current_depth == 0)
-            return Evaluation::evaluate(position);
+        int score;
+        Move move;
+        Result operator-() {return { -score, move };}
+    };
+
+    Move get_hash_move(const Position& position)
+    {
+        Move hash_move;
+        
+        const auto & entry = transposition_table.probe(position.hashkey());
+        if(!entry)
+            return hash_move;
+        if(entry->node_type != TTEntry::NodeType::EXACT)
+            return hash_move;
+        return entry->best_move;
+    }
+
+    Result negamax(Position position, int alpha, int beta, int depth)
+    {
+        if (depth == 0)
+            return {Evaluation::evaluate(position), {}};
 
         MoveList moves;
 
         MoveGenerator move_generator_{&position, &moves};
         move_generator_.gen_all_moves();
 
-        int32_t max_score = MATE_SCORE;
+        MoveOrderer move_orderer{moves, get_hash_move(position)};
+
+        Result best_result {MATE_SCORE, {}};
 
         uint32_t legal_move_counter = 0;
-        for (auto move : moves)
+        while (move_orderer.has_next())
         {
-            irrecoverable_states_[current_depth] = position.irrecoverable_state();
+            Move move = move_orderer.next();
+            
             position.make_move(move);
+
+            Position::IrrecoverableState irrecoverable_state = position.irrecoverable_state();
 
             if (!position.is_in_check(toggle_color(position.side_to_move())))
             {
                 ++legal_move_counter;
-                int32_t score = -negamax_(position, current_depth - 1);
-                if (score > max_score)
-                    max_score = score;
+                auto result = -negamax(position, -beta, -alpha, depth - 1);
+                result.move = move;
+                if (result.score > best_result.score)
+                {
+                    best_result = result;
+                    if( result.score > alpha ) 
+                        alpha = result.score;
+                }
+                if(result.score >= beta)
+                    return best_result;
             }
-            position.unmake_move(move, irrecoverable_states_[current_depth]);
+
+            position.unmake_move(move, irrecoverable_state);
         }
 
         if (legal_move_counter == 0)
         {
             if (position.is_in_check(position.side_to_move()))
-                return MATE_SCORE - SEARCH_DEPTH + current_depth;
-            return STALEMATE_SCORE;
+                best_result.score =  MATE_SCORE - SEARCH_DEPTH + depth;
+            else
+                best_result.score = STALEMATE_SCORE;
         }
 
-        return max_score;
+        TTEntry tt_entry = {.key = position.hashkey(), 
+                            .best_move = best_result.move, 
+                            .depth = depth,
+                            .score = best_result.score,
+                            .node_type = TTEntry::NodeType::EXACT};
+                    transposition_table.insert(std::move(tt_entry));
+
+        return best_result;
     }
 
-    Move search(Position &position)
+    Move iterative_deepening_search(Position &position)
     {
-        MoveList moves;
+        Result result;
 
-        MoveGenerator move_generator_{&position, &moves};
-        move_generator_.gen_all_moves();
-
-        int32_t max_score = MATE_SCORE;
-        Move best_move;
-
-        for (auto move : moves)
+        for(unsigned depth = 1; depth <= SEARCH_DEPTH; ++depth)
         {
-            irrecoverable_states_[SEARCH_DEPTH] = position.irrecoverable_state();
-            position.make_move(move);
-
-            if (!position.is_in_check(toggle_color(position.side_to_move())))
-            {
-                int32_t score = -negamax_(position, SEARCH_DEPTH - 1);
-                if (score > max_score)
-                {
-                    max_score = score;
-                    best_move = move;
-                }
-            }
-
-            position.unmake_move(move, irrecoverable_states_[SEARCH_DEPTH]);
+            result = negamax(position, MATE_SCORE, -MATE_SCORE, depth);
+            std::cout << std::format("[Debug] Depth: {}, Move: {}, Score: {}", depth, result.move.uci_notation(), result.score) << std::endl;
         }
-        return best_move;
+        return result.move;
     }
 }
