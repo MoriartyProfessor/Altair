7ecc73e92d110d1e7d0bd1ebd28844c0b3e4cc2a
MoriartyProfessor <farididdin456@gmail.com>
2026-03-10 02:10:35 +0500

    Perft use make/unmake.

--- src/perft.cpp
+++ src/perft.cpp
@@ -5,8 +5,6 @@
 #include "bitboard.hpp"
 #include <iostream>
 
-Position::IrrecoverableState irrecoverable_states_[MAX_DEPTH];
-
 uint64_t Perft::test_copy(Position position, uint32_t depth)
 {
     MoveList moves;
@@ -37,11 +35,11 @@ uint64_t Perft::test_make_unmake(Position& position, uint32_t depth)
     uint64_t node_count = 0;
     for (auto move : moves)
     {
-        irrecoverable_states_[depth] = position.irrecoverable_state();
+        auto irrecoverable_state = position.irrecoverable_state();
         position.make_move(move);
         if (!position.is_in_check(toggle_color(position.side_to_move())))
             node_count += test_make_unmake(position, depth - 1);
-        position.unmake_move(move, irrecoverable_states_[depth]);
+        position.unmake_move(move, irrecoverable_state);
     }
     return node_count;
 }
@@ -71,10 +69,10 @@ void Perft::thorough_test(Position &position, Result &result, Move move, uint32_
 
     for (auto move : moves)
     {
-        irrecoverable_states_[depth] = position.irrecoverable_state();
+        auto irrecoverable_state = position.irrecoverable_state();
         position.make_move(move);
         if (!position.is_in_check(toggle_color(position.side_to_move())))
             thorough_test(position, result, move, depth - 1);
-        position.unmake_move(move, irrecoverable_states_[depth]);
+        position.unmake_move(move, irrecoverable_state);
     }
 }
