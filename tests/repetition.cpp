0b4a18e83882f3749d71a1412f2172e455e61c14
MoriartyProfessor <farididdin456@gmail.com>
2026-03-09 19:28:49 +0500

    Basic repetition test.

---
+++ tests/repetition.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include "bitboard.hpp"
+#include "rays.hpp"
+#include "patterns.hpp"
+#include "position.hpp"
+#include "search.cpp"
+
+TEST(repetition, basic) 
+{
+    Rays::init();
+    Patterns::init();
+    Zobrist::init();
+
+    Position position;
+    Search::update_history_stack(position.hashkey(), position.halfclock());
+
+    Move move{SQ_B1, SQ_C3};
+    move.set_piece_type(KNIGHT);
+    move.set_quiet();
+    
+    position.make_move(move);
+    Search::update_history_stack(position.hashkey(), position.halfclock());
+
+    move.set_from(SQ_B8);
+    move.set_to(SQ_C6);
+    position.make_move(move);
+    Search::update_history_stack(position.hashkey(), position.halfclock());
+
+    EXPECT_EQ(Search::was_position_reached(Search::history_stack, position.halfclock()), false);
+
+    move.set_from(SQ_C3);
+    move.set_to(SQ_B1);
+    position.make_move(move);
+    Search::update_history_stack(position.hashkey(), position.halfclock());
+
+    EXPECT_EQ(Search::was_position_reached(Search::history_stack, position.halfclock()), false);
+
+    move.set_from(SQ_C6);
+    move.set_to(SQ_B8);
+    position.make_move(move);
+    Search::update_history_stack(position.hashkey(), position.halfclock());
+    EXPECT_EQ(Search::was_position_reached(Search::history_stack, position.halfclock()), true);
+}
