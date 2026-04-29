b565b1b9da14a261c10c2d19e20785d3c775bece
MoriartyProfessor <farididdin456@gmail.com>
2026-04-29 14:54:52 +0500

    Update version.

--- src/engineinfo.hpp
+++ src/engineinfo.hpp
@@ -10,8 +10,8 @@ namespace EngineInfo {
     constexpr std::string_view AUTHOR = "Nickname";
     constexpr std::string_view NAME = "Gwaihir";
     constexpr uint8_t MAJOR = 0;
-    constexpr uint8_t MINOR = 4;
-    constexpr uint8_t PATCH = 2;
+    constexpr uint8_t MINOR = 5;
+    constexpr uint8_t PATCH = 0;
 
     std::string engine_id() {
         return std::format("{}-{}.{}.{}", NAME, MAJOR, MINOR, PATCH);
